0b1ea6cdcfccce1b29d712ba32587bedf5b2c087
Drk-Raven <chikkandarsheik@gmail.com>
2022-02-18 13:38:36 +0530

    This is a demo project

---
+++ script.js
@@ -0,0 +1,7 @@
+function catGenerator (){
+var image = document.createElement('img');
+var div = document.getElementById('cat-gen');
+image.src = "https://cdn2.thecatapi.com/images/1al.gif";
+div.appendChild(image);
+
+}
